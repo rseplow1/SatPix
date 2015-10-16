cbe818595c2272dee34f6954190c5d1707e88d88
Rachel Seplow <rseplow1@hwemail.com>
2015-10-15 22:37:54 -0700

    new version!

--- SatPix/src/SatPix.java
+++ SatPix/src/SatPix.java
@@ -6,20 +6,31 @@ import java.util.Scanner;
 
 		public static void main(String[] args) throws IOException
 		{
-//			boolean[][] booleanArr = fileToBoolArray("satpix.in");
-			int sizeOfLargestPasture;
-			
-			/* YOUR CODE GOES HERE */
-			
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
-			out.println(sizeOfLargestPasture);
-			out.close();
+		boolean[][] booleanArr = fileToBoolArray("satpix.txt");
+			int sizeOfLargestPasture=0;
+			int currentPasture=0;
+			for (int i = 0; i<booleanArr.length; i++)
+			{
+				for (int j = 0; j<booleanArr[0].length; j++)
+				{
+					currentPasture = SatPix.recursivelyMeasureAndMarkPasture(booleanArr.length, booleanArr[0].length, booleanArr);
+					if (currentPasture > sizeOfLargestPasture)
+					{
+						sizeOfLargestPasture = currentPasture;
+					}
+				}
+			}
+			System.out.println (sizeOfLargestPasture);
+			//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
+		//	out.println(sizeOfLargestPasture);
+			//out.close();
 			}
 		
 	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException
 	{
 		//This helper method converts the input file into a 2D array of booleans
-		Scanner sc = new Scanner (System.in);
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner (new File(fileName));
 		int c = sc.nextInt();
 		int r = sc.nextInt();
 		boolean [] [] boolArr =  new boolean [c][r];
@@ -44,7 +55,32 @@ import java.util.Scanner;
 		{
 			//This recursive method employs the flood-fill algorithm to
 			//count the size of a single pasture and "mark" it so it is not double-counted
-			return 0;
+			if (arr[row][col] != true)
+			{
+				return 0;
+			}
+			arr[row][col] = false;
+			if (row == arr.length || col == arr[0].length)
+			{
+				return 1;
+			}
+			if (arr[row+1][col] == true)
+			{
+				recursivelyMeasureAndMarkPasture(row+1, col, arr);
+			}
+			if (arr[row-1][col]== true)
+			{
+				recursivelyMeasureAndMarkPasture(row-1, col, arr);
+			}
+			if (arr[row][col+1]==true)
+			{
+				recursivelyMeasureAndMarkPasture(row, col+1, arr);
+			}
+			if (arr[row][col-1] == true)
+			{
+				recursivelyMeasureAndMarkPasture(row, col-1, arr);
+			}
+			return 1;
 		}
 		
 	}
