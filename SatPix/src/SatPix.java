1ed452c1e92170046516e6fe754a5693617c9e55
Rachel Seplow <rseplow1@hwemail.com>
2015-10-16 21:31:03 -0700

    Parts 1-3 are done!

--- SatPix/src/SatPix.java
+++ SatPix/src/SatPix.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 		public static void main(String[] args) throws IOException
 		{
-		boolean[][] booleanArr = fileToBoolArray("satpix.txt");
+			boolean[][] booleanArr = fileToBoolArray("satpix.txt");
 			int sizeOfLargestPasture=0;
 			int currentPasture=0;
-			for (int i = 0; i<booleanArr.length; i++)
+		    for (int i = 0; i<booleanArr.length-1; i++)
 			{
-				for (int j = 0; j<booleanArr[0].length; j++)
+				for (int j = 0; j<booleanArr[0].length-1; j++)
 				{
-					currentPasture = SatPix.recursivelyMeasureAndMarkPasture(booleanArr.length, booleanArr[0].length, booleanArr);
+					currentPasture = SatPix.recursivelyMeasureAndMarkPasture(i, j, booleanArr);
 					if (currentPasture > sizeOfLargestPasture)
 					{
 						sizeOfLargestPasture = currentPasture;
@@ -21,9 +21,18 @@ import java.util.Scanner;
 				}
 			}
 			System.out.println (sizeOfLargestPasture);
-			//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
-		//	out.println(sizeOfLargestPasture);
-			//out.close();
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
+		out.println(sizeOfLargestPasture);
+		out.close();
+			
+		//	for (int i = 0; i<booleanArr.length; i++)
+		//	{
+			//	for (int j =0; j<booleanArr[0].length; j++)
+		//		{
+		//			System.out.print (" " + booleanArr[i][j]);
+		//		}
+		//		System.out.println (" ");
+		//	}
 			}
 		
 	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException
@@ -33,54 +42,51 @@ import java.util.Scanner;
 		Scanner sc = new Scanner (new File(fileName));
 		int c = sc.nextInt();
 		int r = sc.nextInt();
-		boolean [] [] boolArr =  new boolean [c][r];
-		for (int i = 0; i<c; i++)
+		String s = "";
+		boolean [] [] boolArr =  new boolean [r][c];
+		for (int i = 0; i<r; i++)
 		{
-			for (int j = 0; j<r; j++)
+			if (sc.hasNext() == true)
+			 {
+				s = sc.next();
+			 }
+			for (int j = 0; j<c; j++)
 			{
-				if (sc.next() == "*")
+				String sub = s.substring(j, j+1);
+				if (sub.equals ( "*"))
 				{
-					boolArr [c][r] =  true;
+					boolArr [i][j] =  true;
 				}
-				else if (sc.next() == ".")
+				else if (sub.equals ("."))
 				{
-					boolArr [c][r] = false;
+					boolArr [i][j] = false;
 				}
 			}
 		}
 		return boolArr;
+		
 	}
 
 		private static int recursivelyMeasureAndMarkPasture(int row, int col, boolean[][] arr)
 		{
+			if (row >= 0
+					&& col >= 0
+					&& row < arr.length
+					&& col < arr[0].length
+					&& arr[row][col] == true)
+			{
+				arr[row][col] = false;
+			
+				return 1 + recursivelyMeasureAndMarkPasture(row - 1, col, arr)
+				+ recursivelyMeasureAndMarkPasture(row + 1, col, arr)
+				+ recursivelyMeasureAndMarkPasture(row, col + 1, arr)
+				+ recursivelyMeasureAndMarkPasture(row, col - 1, arr);
+				
+			}
+			return 0;
 			//This recursive method employs the flood-fill algorithm to
 			//count the size of a single pasture and "mark" it so it is not double-counted
-			if (arr[row][col] != true)
-			{
-				return 0;
-			}
-			arr[row][col] = false;
-			if (row == arr.length || col == arr[0].length)
-			{
-				return 1;
-			}
-			if (arr[row+1][col] == true)
-			{
-				recursivelyMeasureAndMarkPasture(row+1, col, arr);
-			}
-			if (arr[row-1][col]== true)
-			{
-				recursivelyMeasureAndMarkPasture(row-1, col, arr);
-			}
-			if (arr[row][col+1]==true)
-			{
-				recursivelyMeasureAndMarkPasture(row, col+1, arr);
-			}
-			if (arr[row][col-1] == true)
-			{
-				recursivelyMeasureAndMarkPasture(row, col-1, arr);
-			}
-			return 1;
+		
 		}
 		
 	}
